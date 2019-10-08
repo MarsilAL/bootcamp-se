fd74378acd1b913af97bb609d83fa243c4ab6a54
Alashkar <alashkar.marsil@gmail.com>
2019-10-08 13:06:41 +0200

    add rot13

--- src/rot13.js
+++ src/rot13.js
@@ -1,11 +1,22 @@
 
-function rot13_encode(input) {
-    return "this is the encoded value of " + input
+function rot13_encode(str) {
+    var input     = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789 ';
+    var output    = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm123456789 ';
+    var index     = x => input.indexOf(x);
+    var translate = x => index(x) > -1 ? output[index(x)] : x;
+    return str.split('').map(translate).join('');
 }
 
-function rot13_decode(input) {
-    return "this is the decoded value of " + input
+
+function rot13_decode(str) {
+    var output     = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789 ';
+    var  input   = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm123456789 ';
+    var index     = x => input.indexOf(x);
+    var translate = x => index(x) > -1 ? output[index(x)] : x;
+    return str.split('').map(translate).join('');
 }
+
+
 
 
 export {rot13_encode, rot13_decode};
